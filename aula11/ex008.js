af71bedd16cc7c335f133a2fbb1c67fe0c8c42c2
Gabriel Vasconcelos Santos <108894922+GabVS4@users.noreply.github.com>
2026-08-21 13:51:38 -0300

    if e else no JavaScript

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 12
+
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+
+if (vel > 60){ //Condição simples!
+    console.log(`MULTADO!`)
+}
+console.log('Velocidade Permitida')
+
+
