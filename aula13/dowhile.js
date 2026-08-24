43afc57ef0579c69f0ffdccfe93c22504c68d8f5
Gabriel Vasconcelos Santos <108894922+GabVS4@users.noreply.github.com>
2026-08-24 13:39:38 -0300

    while e do while

---
+++ aula13/dowhile.js
@@ -0,0 +1,6 @@
+var contador = 0
+
+do {
+    console.log(`Passo: ${contador}`)
+    contador++
+} while (contador <= 10)
---
+++ aula13/while.js
@@ -0,0 +1,6 @@
+var contador = 0
+
+while (contador <= 10) {
+    console.log(`Passo: ${contador}`)
+    contador++
+}
