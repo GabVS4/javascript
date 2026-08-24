2a9a612057803cc1ebb362a89b457f67abf6b0e4
Gabriel Vasconcelos Santos <108894922+GabVS4@users.noreply.github.com>
2026-08-24 13:57:48 -0300

    estrutura de repetição com variável de controle (for)

---
+++ aula14/for.js
@@ -0,0 +1,15 @@
+/*
+var contador = 1
+
+while (contador <= 15) {
+    console.log(contador)
+    contador++
+}
+*/
+console.log('Começando...')
+
+for (var contador = 1; contador <= 15; contador++){
+    console.log(contador)
+}
+
+console.log('Terminou!')
